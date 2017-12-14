f91f65c3f68162862f2cd9bcb9dd625a3d3d1c49
ShenHaoWen <805154269@qq.com>
2017-12-14 16:48:30 +0800

    Initial Commit

---
+++ NSImageText/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSImageText
+//
+//  Created by Karthus on 2017/12/14.
+//  Copyright © 2017年 karthus. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ NSImageText/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  NSImageText
+//
+//  Created by Karthus on 2017/12/14.
+//  Copyright © 2017年 karthus. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
